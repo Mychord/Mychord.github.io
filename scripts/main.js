a57f169911fd6c5a3bd8749d636d72fdf2553465
Mychord <q1602280368@163.com>
2023-06-19 14:24:42 +0800

    add music

--- scripts/main.js
+++ scripts/main.js
@@ -12,3 +12,17 @@ function removeActiveClasses() {
     panel.classList.remove("active");
   })
 }
+
+function randomSelect(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+const audioHrefs = ["audios/recreate.mp3", "audios/Scarborough Fair.mp3", "audios/starfall.mp3"];
+const audioName = document.querySelector("#audioName");
+const audioSrc = document.querySelector("#audioSrc");
+const audioHref = document.querySelector("#audioHref");
+
+const randomAudio = randomSelect(audioHrefs);
+audioName.innerHTML = randomAudio.split("/")[1];
+audioSrc.setAttribute("src", randomAudio);
+audioHref.setAttribute("href", randomAudio);
