d4b5f9a1fc55b7dc154b75bbcfcb334f3ecd19dd
Mychord <q1602280368@163.com>
2023-06-24 15:31:18 +0800

    add snow function

--- scripts/main.js
+++ scripts/main.js
@@ -2,10 +2,10 @@
 let clickCount = 0;
 
 // 开始按钮
-const startButton =  document.getElementById("start");
+const startButton = document.getElementById("start-button");
 // 为开始按钮添加点击事件
 startButton.addEventListener("click", () => {
-  if(clickCount < 4) window.location.href = "normal.html";
+  if (clickCount < 4) window.location.href = "normal.html";
   else window.location.href = "secret.html";
 });
 
@@ -17,8 +17,8 @@ function randomLocation() {
     return 0.3 * Math.random() + (Math.random() < 0.5 ? 0.1 : 0.6);
   };
 
-  const randomLeft = Math.round(window.innerWidth * randomNum()) + "px";
-  const randomTop = Math.round(window.innerHeight * randomNum()) + "px";
+  const randomLeft = `${Math.round(window.innerWidth * randomNum())}px`;
+  const randomTop = `${Math.round(window.innerHeight * randomNum())}px`;
 
   return [randomLeft, randomTop];
 }
@@ -33,12 +33,64 @@ document.body.append(vortex);
 // 为漩涡图片添加点击事件
 vortex.addEventListener("click", () => {
   clickCount++;
-  if(clickCount == 5) {
+  if (clickCount == 5) {
     vortex.remove();
     document.body.style.backgroundColor = "black";
     startButton.classList.add("secret");
+    snow();
   }
   else {
     [vortex.style.left, vortex.style.top] = randomLocation();
   }
-})
+});
+
+// 雪花对象
+class Snowflake {
+  constructor(el) {
+    // 下降速度
+    this.speed = 20;
+    // 初始位置
+    this.left = Math.round(Math.random() * window.innerWidth);
+    this.top = 0;
+    // 绑定元素
+    this.el = el;
+    // 标识时间间隔的 id
+    this.intervalId = null;
+  }
+  fall() {
+    // 设置初始位置
+    this.el.style.left = `${this.left}px`;
+    this.el.style.top = `${this.top}px`;
+    // 雪花透明度
+    this.el.style.opacity = (Math.round(Math.random() * 4) + 6) / 10;
+    // 雪花大小
+    this.el.style.fontSize = `${16 + Math.round(Math.random() * 20)}px`
+    this.intervalId = setInterval(() => {
+      this.falling();
+    }, 200);
+  }
+  falling() {
+    if(this.left < window.innerWidth && this.left >= 0 && this.top < window.innerHeight) {
+      this.left += Math.round(Math.random() > 0.5 ? Math.random() * 10 : -Math.random() * 10);
+      this.top += this.speed;
+      this.el.style.left = `${this.left}px`;
+      this.el.style.top = `${this.top}px`;
+    }
+    else {
+      clearInterval(this.intervalId);
+      this.el.remove();
+    }
+  }
+}
+
+// 雪花特效
+function snow() {
+  setInterval(() => {
+    const div = document.createElement("div");
+    div.innerHTML = "❄";
+    div.className = "snowflake";
+    const snowflake = new Snowflake(div);
+    document.body.append(div);
+    snowflake.fall();
+  }, 400);
+}
--- scripts/secret.js
+++ scripts/secret.js
@@ -1,3 +1,4 @@
+// 图片集合
 const panels = document.querySelectorAll(".panel");
 
 panels.forEach(panel => {
@@ -13,14 +14,28 @@ function removeActiveClasses() {
   })
 }
 
+// 音乐集合
+const audioSrcs = ["audios/recreate.mp3", "audios/Scarborough Fair.mp3", "audios/starfall.mp3", "audios/desire.mp3"];
+const audioName = document.querySelector("#audio-name");
+const audioSrc = document.querySelector("#audio-src");
+randomMusic();
+
+// 随机选取数组元素
 function randomSelect(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
-const audioSrcs = ["audios/recreate.mp3", "audios/Scarborough Fair.mp3", "audios/starfall.mp3", "audios/desire.mp3"];
-const audioName = document.querySelector("#audio-name");
-const audioSrc = document.querySelector("#audio-src");
+// 随机选择一首音乐播放
+function randomMusic() {
+  let randomAudio = randomSelect(audioSrcs);
+  // 避免随机播放的音乐与正在播放的音乐相同
+  while(randomAudio == audioSrc.getAttribute("src")) {
+    randomAudio = randomSelect(audioSrcs);
+  }
+  audioName.innerHTML = randomAudio.split("/")[1];
+  audioSrc.setAttribute("src", randomAudio);
+}
 
-const randomAudio = randomSelect(audioSrcs);
-audioName.innerHTML = randomAudio.split("/")[1];
-audioSrc.setAttribute("src", randomAudio);
+// 随机播放音乐按钮
+const refreshButton = document.querySelector(".refresh-button");
+refreshButton.addEventListener("click", randomMusic);
